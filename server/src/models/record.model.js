f29ee24c4097895fc8c28f519e1902a6839abc00
dev.langesh <dev.langesh@gmail.com>
2022-12-10 23:27:09 +0530

    model update with faculty and student record

---
+++ server/src/models/record.model.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const studentRecordSchema = mongoose.Schema({
+  student_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "student",
+  },
+  purpose: String,
+  in_time: String,
+  out_time: String,
+  date: String,
+});
+
+const facultyRecordSchema = mongoose.Schema({
+  faculty_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "faculty",
+  },
+  purpose: String,
+  in_time: String,
+  out_time: String,
+  date: String,
+});
+
+const studentRecord = mongoose.model("student-record", studentRecordSchema);
+const facultyRecord = mongoose.model("faculty-record", facultyRecordSchema);
+
+module.exports = { studentRecord, facultyRecord };
--- server/src/models/student.model.js
+++ server/src/models/student.model.js
@@ -1,12 +1,28 @@
 const mongoose = require("mongoose");
 
-const studentSchema = mongoose.Schema({
-  reg_no: { type: String, required: true, unique: true },
-  name: { type: String, required: true },
-  department: String,
-  year: String,
-});
+const studentSchema = mongoose.Schema(
+  {
+    reg_no: { type: String, required: true, unique: true },
+    name: { type: String, required: true },
+    department: String,
+    year: String,
+  },
+  {
+    collection: "students",
+  }
+);
+
+const facultySchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    department: String,
+  },
+  {
+    collection: "faculties",
+  }
+);
 
 const Student = mongoose.model("student", studentSchema);
+const Faculty = mongoose.model("faculty", facultySchema);
 
-module.exports = { Student };
+module.exports = { Student, Faculty };

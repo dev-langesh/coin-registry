e2c4151e34ee593b31b9f820ca71ed63b74b9f42
dev.langesh <dev.langesh@gmail.com>
2022-12-11 11:57:50 +0530

    get student and faculty details

--- server/src/routes/register/register.route.js
+++ server/src/routes/register/register.route.js
@@ -2,6 +2,8 @@ const { register } = require("./register.controller");
 
 const registerRouter = require("express").Router();
 
+registerRouter.get("/records");
+
 registerRouter.post("/register", register);
 
 module.exports = { registerRouter };

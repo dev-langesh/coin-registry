37adc66eafeb85e20e598daa417156ec56033597
dev.langesh <dev.langesh@gmail.com>
2022-12-12 08:37:55 +0530

    deploy bug fix

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,4 @@
-const http = require("node:http");
+const http = require("http");
 const { app } = require("./app");
 const { connectDb } = require("./config/connectDb");
 const { configEnv } = require("./config/envConf");
